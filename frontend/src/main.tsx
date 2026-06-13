1c46cbd7ddb54ce90f8a96a0cd89cc145bf2b5b8
jade <jadesada.jung@gmail.com>
2026-06-14 03:18:18 +0700

    update app , main ,config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryclient.ts'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './hooks/useAuth.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode >,
 )
